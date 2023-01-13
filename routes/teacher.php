84516cc0e20d04304abf70eaa00c1982a6b2e7cb
Ziad-Abazed <108790293+Ziad-Abazed@users.noreply.github.com>
2023-01-13 19:34:38 +0300

    Setting_teacher_dashboard

---
+++ routes/teacher.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+/*
+|--------------------------------------------------------------------------
+| student Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//==============================Translate all pages============================
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'auth:teacher']
+    ], function () {
+
+    //==============================dashboard============================
+    Route::get('/teacher/dashboard', function () {
+
+        $ids = Teacher::findorFail(auth()->user()->id)->Sections()->pluck('section_id');
+        $data['count_sections']= $ids->count();
+        $data['count_students']= \App\Models\Student::whereIn('section_id',$ids)->count();
+        return view('pages.Teachers.dashboard.dashboard',$data);
+    });
+
+});
